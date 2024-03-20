c6f3dc27295db696333433251071591dbc53908b
Dally <dallymaminomenjanahar@gmail.com>
2024-03-20 23:51:34 +0300

    modif component admin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,39 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HommeComponent } from './homme/homme.component';
 import { GuideComponent } from './guide/guide.component';
 import { MapComponent } from './map/map.component';
 import { AboutComponent } from './about/about.component';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AdminComponent } from './admin/admin.component';
+import { LoginComponent } from './login/login.component';
+import { Router } from '@angular/router';
+
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, HommeComponent, GuideComponent, MapComponent, AboutComponent, NavbarComponent, AdminComponent,  RouterOutlet],
+  imports: [CommonModule, HommeComponent, GuideComponent, MapComponent, AboutComponent, NavbarComponent, AdminComponent, LoginComponent,  RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  isLoginPage: boolean = false;
+  isAdminpage: boolean = false;
+
+
+  constructor(private router: Router){}
+
+  ngOnInit(){
+    this.router.events.subscribe((event) =>{
+      if (event instanceof NavigationEnd){
+        this.isLoginPage = event.url === '/login';
+        this.isAdminpage = event.url === '/admin';
+      }
+    })
+  }
   
 }
 
